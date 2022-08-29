1445b3cc428f1c1d1fe55807162b537548cc0d3f
Chris Oliver <excid3@gmail.com>
2022-08-29 14:35:10 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :jobs
-
-  get '/privacy', to: 'home#privacy'
-  get '/terms', to: 'home#terms'
-
-authenticate :user, lambda { |u| u.admin? } do
   mount Sidekiq::Web => '/sidekiq'
+  resources :jobs
+  
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  namespace :madmin do
-    resources :impersonates do
-      post :impersonate, on: :member
-      post :stop_impersonating, on: :collection
+  authenticate :user, lambda { |u| u.admin? } do
+    namespace :madmin do
+      resources :impersonates do
+        post :impersonate, on: :member
+        post :stop_impersonating, on: :collection
+      end
     end
   end
-end
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'home#index'
 end
